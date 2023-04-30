70ab9ad61be6e0f8c7602257f171d18e0a4760f8
Shawn Yang <yangshawn14@gmail.com>
2023-04-29 21:06:10 -0600

    Add js files

---
+++ lesson2/scripts/index.js
@@ -0,0 +1,11 @@
+let dateModif = document.querySelector("#date");
+let year = document.querySelector("#year");
+
+const d = new Date();
+let currYear = d.getFullYear();
+
+const lastModif = document.lastModified;
+
+year.innerHTML = `&#169; ${currYear} .:|:. Shawn Yang .:|:. Idaho`;
+dateModif.innerHTML = `Last updated ${lastModif}`;
+
