23f67a946ef35a6982eee8e46e483961506c10ca
Shawn Yang <yangshawn14@gmail.com>
2023-09-21 10:23:13 -0600

    Fix and restyle footer

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
-let dateModif = document.querySelector("#date");
+let dateModif = document.querySelector("#lastModified");
 let year = document.querySelector("#year");
 
 const d = new Date();
@@ -6,6 +6,6 @@ let currYear = d.getFullYear();
 
 const lastModif = document.lastModified;
 
-year.innerHTML = `&#169; ${currYear} .:|:. Shawn Yang .:|:. Idaho`;
+year.innerHTML = `&#169; ${currYear} <br/> Shawn Yang <br/> Idaho`;
 dateModif.innerHTML = `Last updated ${lastModif}`;
 
