ed5c65845f1f3ae8dc9b5e580f1f24092cd327a5
Shawn Yang <yangshawn14@gmail.com>
2023-10-05 19:47:14 -0600

    Add hamburger button

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -9,3 +9,11 @@ const lastModif = document.lastModified;
 year.innerHTML = currYear;
 dateModif.innerHTML = `Last updated ${lastModif}`;
 
+// Add open class to menu button
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav ul');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+}); 
