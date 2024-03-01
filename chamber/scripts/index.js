6b70dd7a1ffce9cd78834a10aaa03f260bee4189
Shawn Yang <yangshawn14@gmail.com>
2024-03-01 00:37:23 -0700

    Add dark mode

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -13,6 +13,16 @@ hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle('open');
 });
 
+// Add dark-mode class to body and main
+const modeButton = document.querySelector(".slider");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	body.classList.toggle('dark-mode');
+	main.classList.toggle('dark-mode');
+});
+
 // Footer
 let dateModif = document.querySelector("#dateModif");
 let year = document.querySelector("#year");
