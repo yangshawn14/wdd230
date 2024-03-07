0c3317ad88951707f3d9819a93ab082a2e353fb7
Shawn Yang <yangshawn14@gmail.com>
2024-03-06 18:14:28 -0700

    fix url typo

--- scripts/links.js
+++ scripts/links.js
@@ -1,1 +1,33 @@
-const baseURL = "https://yourgithubusername.github.io/wdd230/";
+const baseURL = "https://yangshawn14.github.io/wdd230/";
+const linksURL = "https://yangshawn14.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+}
+
+const displayLinks = (weeks) => {
+    const activities = document.querySelector(".activities");
+    const ol = activities.querySelector("ol");
+
+    weeks.lessons.forEach(week => {
+        const weekListItem = document.createElement('li');
+        weekListItem.textContent = `Lesson ${week.lesson}`;
+
+        week.links.forEach(link => {
+            const linkElement = document.createElement('a');
+            linkElement.href = `${baseURL}${link.url}`;
+            linkElement.textContent = `${link.title}`;
+
+            const separator = document.createTextNode(' | ');
+
+            weekListItem.appendChild(separator);
+            weekListItem.appendChild(linkElement);
+        });
+        ol.appendChild(weekListItem);
+
+    });
+}
+
+getLinks()
